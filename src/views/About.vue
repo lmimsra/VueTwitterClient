19a0d6b469a83ec5c4055cd50b1fe5cc49b4f5c6
mmr.K <lmimsra07@gmail.com>
2018-07-11 20:32:46 +0900

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,26 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+  import Twitter from 'twitter'
+
+  var client = new Twitter({
+      consumer_key: 'oWeRDDxHH86txvuM2rPwTFRW1',
+      consumer_secret: 'w1YjC2cIzflnasYhKlwFhdJbesPeDSulvkeJzSTYiaqRu9bXNk',
+      access_token_key: '1285788504-A0fpn28fTCxtk4YNh2ZVfv9yUD6f11KPIX2xQt7',
+      access_token_secret: 'bw6rh9pki5Fu0CkgWz3dcrQpnEyKPBT8oFvlXlFE3kG9X'
+  });
+  var params = {screen_name: 'nodejs'};
+
+  client.get('statuses/user_timeline', params, function(error, tweets, response){
+      if (!error) {
+          console.log(tweets);
+      }
+  });
+
+  export default {
+      name: 'about',
+
+  }
+</script>
